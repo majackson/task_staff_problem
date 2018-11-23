598b7359ab84c785e8c596f23c1807e700ac4d7b
Rehunathan, Devan <devan.rehunathan@sky.uk>
2018-11-23 14:25:25 +0000

    All tests pass

--- staff.py
+++ staff.py
@@ -6,18 +6,28 @@ from collections import defaultdict
 
 class CompanyHierarchy:
     def __init__(self):
-        self.dict = defaultdict(list)
+        self.manager_dict = defaultdict(list)
+        self.managee_dict = defaultdict(list)
 
     def add(self, line):
         line = line.replace("\n", "")
         managee, manager = line.split(" ")
-        self.dict[managee].append(manager)
+
+        self.manager_dict[managee].append(manager)
+
+        self.managee_dict[manager].append(managee)
+
+        # Resolve managers
+        if managee in self.managee_dict.keys():
+            for those_managed in self.managee_dict[managee]:
+                self.manager_dict[those_managed].append(manager)
+
 
     def length(self):
-        return len(self.dict)
+        return len(self.manager_dict)
 
     def keys(self):
-        return self.dict.keys()
+        return self.manager_dict.keys()
 
 
 def read_file(input_file):
@@ -38,9 +48,10 @@ if __name__ == "__main__":
     first_employee = sys.argv[2]
     second_employee = sys.argv[3]
 
-    first_employee_managers = company.dict[first_employee]
-    second_employee_managers = company.dict[second_employee]
+    first_employee_managers = company.manager_dict[first_employee]
+    second_employee_managers = company.manager_dict[second_employee]
 
-    common_managers = list(set(first_employee_managers).intersection(second_employee_managers))
+    # Preserve Order of intersection
+    common_managers = [i for i in first_employee_managers if i in second_employee_managers]
     if len(common_managers)>0:
         print common_managers[0]
