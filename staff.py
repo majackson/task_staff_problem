62b205620577d9228cd6557b6368debae2e3459a
Rehunathan, Devan <devan.rehunathan@sky.uk>
2018-11-23 12:44:46 +0000

    Added function to read input file

--- staff.py
+++ staff.py
@@ -2,5 +2,14 @@
 
 import sys
 
+def read_file(input_file):
+    """
+    Read input file, into key, value pairs.
+    """
+    with open(input_file, "r") as infile:
+        for line in infile:
+            managee_name, manager_name = line.split(" ")
+            print managee_name, manager_name
+
 if __name__ == "__main__":
-    pass
+    read_file(sys.argv[1])
