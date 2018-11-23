ccecfcdedf9cd083de9b8e3574b825d1c47d4f82
Rehunathan, Devan <devan.rehunathan@sky.uk>
2018-11-23 13:04:45 +0000

    Updted tests from README

--- tests.py
+++ tests.py
@@ -8,6 +8,14 @@ COMMAND = "./staff.py"
 INPUT = "staff.input"
 
 
-def test_read_file():
-    output = subprocess.check_output([COMMAND, INPUT, "John", "Jane"])
-    assert output == "John\n"
+def test_script():
+    output = subprocess.check_output([COMMAND, INPUT, "Adam", "Andrea"])
+    assert output == "Bob\n"
+
+    output = subprocess.check_output([COMMAND, INPUT, "Adam", "Aisha"])
+    assert output == "Chris\n"
+
+    output = subprocess.check_output([COMMAND, INPUT, "Brenda", "Andrea"])
+    assert output == "Chris\n"
+
+
